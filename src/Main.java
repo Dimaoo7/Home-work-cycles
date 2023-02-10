29791d404910431bdd0eb37071fd9ad9ab7be65b
Dimaoo7 <dimakrjukov@gmail.com>
2023-02-10 15:20:54 +0100

    Homework done well

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1() {
@@ -17,7 +22,7 @@ public class Main {
 
     }
 
-    public static void task2() {
+    public static void task2() { //ЭТАААА
         System.out.println("Задача 2");
         int numba = 1;
         while (numba <= 10) {
@@ -31,4 +36,66 @@ public class Main {
         }
         System.out.println(" ");
     }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        int country = 12_000_000;
+        for (int i = 1; i <= 10; i++) {
+            country = country + 11_892;
+            System.out.println("Год " + i + ", численность населения составляет " + country);
+
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4 и 5");
+        int salary = 15_000;
+        int percent = (15_000 / 100) * 7;
+        int result = 0;
+        int sum = 0;
+        int month = 0;
+        while (sum <= 12_000_000) {
+            month = month + 6;
+            result =salary + percent;
+            sum = sum + (result * 6);
+            System.out.println(month + " " + sum);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 6");
+        int salary = 15_000;
+        int percent = (15_000 / 100) * 7;
+        int result = 0;
+        int month = 0;
+        int sum =0;
+        while (month <= 108) {
+            month = month + 6;
+            result =salary + percent;
+            sum = sum + (result * 6);
+            System.out.println(month + " " + sum);
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 7");
+        int day = 0;
+        for (int friday = 3; friday <= 31; friday = friday + 7) {
+            day++;
+            System.out.println("Сегодня " + day + " пятница, " + friday + "-е число. Необходимо подготовить отчет");
+        }
+
+    }
+    public static void task7() {
+        System.out.println("Задача 8");
+        int last = 2023 - 200;
+        int next = 2023 + 100;
+
+        for (; last < next; last = last + 79) {
+            System.out.println("Приветик, метеорит будет: " + last);
+        }
+
+    }
+
 }
+
