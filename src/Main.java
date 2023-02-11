058638673527fb5b47ff43c8cab64003817cf7d6
Dimaoo7 <dimakrjukov@gmail.com>
2023-02-11 09:40:14 +0100

    Ohhh peredelal

--- src/Main.java
+++ src/Main.java
@@ -22,7 +22,8 @@ public class Main {
         }
 
     }
-        public static void task2() {
+
+    public static void task2() {
         System.out.println("Задача 2");
         int numba = 1;
         while (numba <= 10) {
@@ -38,11 +39,12 @@ public class Main {
     }
 
     public static void task3() {
+        System.out.println("Задача 3");
         int sumOfPeople = 12_000_000;
         int born = 17;
         int death = 8;
         int diff = born - death;
-        for(int i = 1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             sumOfPeople += sumOfPeople * diff / 1000;
         }
         System.out.println(sumOfPeople);
@@ -51,40 +53,39 @@ public class Main {
 
     public static void task4() {
         System.out.println("Задача 4");
-        int sum = 15_000;
-        int month = 0;
-        while( sum <= 12_000_000){
-            month = month + 1;
-            sum += sum * 0.07;
-            System.out.println( month +" "+sum);
+        double sum = 15_000;
+        int month = 1;
+        while (sum <= 12_000_000) {
+            sum = sum + sum * 0.07;
+            System.out.println(" Месяц " + month + " " + " Сумма " + sum);
+            month++;
         }
     }
+
     public static void task5() {
         System.out.println("Задача 5");
-        int sum = 15_000;
-        int month = 0;
-        int result = 0;
-        while( result <= 12_000_000){
-            month = month + 6;
-            sum += sum * 0.07;
-            result = sum * 6;
-            System.out.println( month +" "+result);
+        double sum = 15_000;
+        int month = 1;
+        while (sum <= 12_000_000) {
+            sum = sum + sum * 0.07;
+            month++;
+            if (month % 6 == 0)
+                System.out.println(" Месяц " + month + " " + " Сумма " + sum);
         }
+
     }
 
     public static void task6() {
         System.out.println("Задача 6");
-        int salary = 15_000;
-        int percent = (15_000 / 100) * 7;
-        int result = 0;
-        int month = 0;
-        int sum =0;
-        while (month <= 108) {
-            month = month + 6;
-            result =salary + percent;
-            sum = sum + (result * 6);
-            System.out.println(month + " " + sum);
+        double sum = 15_000;
+        int month = 1;
+        while (month < 9 * 12) {
+            sum = sum + sum * 0.07;
+            month++;
+            if (month % 6 == 0)
+                System.out.println("Месяц " + month + " Сумма " + sum);
         }
+
     }
 
     public static void task7() {
